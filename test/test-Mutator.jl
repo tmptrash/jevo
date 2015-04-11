98b6d23a027dd4e34bae9356d0b45df94291ccbe
temp <tmptrash@mail.ru>
2015-04-11 22:29:37 +0300

    * sync

--- test/test-Mutator.jl
+++ test/test-Mutator.jl
@@ -6,10 +6,36 @@ module TestMutator
   # TODO: remove this
   using Debug
 
+  #
+  # Returns true if expression is a: var = const|var
+  #
+  function _isSignedVar(expr)
+    typeof(expr) === Expr         &&
+    Script.isSign(expr.args[1])   && 
+    length(expr.args) === 2       && 
+    (typeof(expr.args[2]) === Int || typeof(expr.args[2]) === Symbol)
+  end
+  #
+  # Checks if specified variable assign line is valid. It checks
+  # all possible variants (full, short, with function,...)
+  # @param block Current code block
+  # @param expr Expression without assign var. e.g.: v = ...
+  # In this case without "v".
+  #
+  function _testVar(block::Script.Block, expr::Expr)
+    if typeof(expr) === Int || # var = const
+       _isSignedVar(expr)      # var = sign{const|var}
+      return true
+    end
+
+    false
+  end
+
+
   facts("Testing Mutator._addVar()...") do
     code   = :(function t() end)
     blocks = [Script.Block(
-      Symbol[],
+      [:var2],
       code.args[2],
       Script.Block(
         Symbol[],
@@ -27,13 +53,13 @@ module TestMutator
     #
     # After adding a var assign line it might be only one in block
     #
-    @fact length(code.args[2].args)    => 1
+    @fact length(code.args[2].args)              => 1
     #
     # Before adding there is no variables in current block,
     # so first variable will be "var1"
     #
-    @fact code.args[2].args[1].args[1] => :(var1)
-    @fact code.args[2]
+    @fact code.args[2].args[1].args[1]           => :(var1)
+    @fact _testVar(code.args[2].args[2].args[2]) => true
   end
 
   # facts("Testing 'add' logic of Mutator.mutate()...") do
