383d8e646f27e299cb3cbf968c73e6e6b494fdf7
tmptrash@mail.ru <k2rwO(H@>
2015-06-17 00:37:08 +0300

    * sync

--- client-test.jl
+++ client-test.jl
@@ -1,8 +1,8 @@
 include("Client.jl")
-ct = Client.create(uint16(2000))
+cl = Client.create(uint16(2000))
 function f(ct, ans)
   println(ct)
   println(ans)
 end
-Event.on(ct.observer, "command", f)
-Client.runCmd(ct, rand, 2,3)
+Event.on(cl.observer, "command", f)
+Client.runCmd(cl, rand, 2,3)
