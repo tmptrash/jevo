537233aff7132465faa13d3ba84cabfad22cc860
tmptrash@mail.ru <k2rwO(H@>
2015-07-22 00:32:06 +0300

    * sync

--- WorldWindow.jl
+++ WorldWindow.jl
@@ -7,9 +7,38 @@
 # circles in OpenGL canvas.
 #
 module WorldWindow
+  type Window
+    #
+	# TODO:
+	#
+    win::Tk_Toplevel,
+	#
+	# TODO:
+	#
+	canvas::Canvas,
+	#
+	# TODO:
+	#
+	context::CairoContext
+  end
+  
   function create()
+    win = Toplevel("Test", 400, 200)
+	c = Canvas(win)
+	pack(c, expand=true, fill="both")
+	ctx = getgc(c)
+
+	set_coords(ctx, 0, 0, 400, 200, 0, 399, 0, 199)
+	set_antialias(ctx, 1)
+	set_line_width(ctx, 1)
+	set_source_rgb(ctx, 1, 1, 1)
+	paint(ctx)
   end
 
-  function dot()
+  function dot(ctx::CairoContext, x::Int, y::Int, r::Int, g::Int, b::Int)
+	set_source_rgb(ctx, r, g, b)
+	move_to(ctx, x, y)
+	line_to(ctx, x+1, y)
+	stroke(ctx)
   end
 end
