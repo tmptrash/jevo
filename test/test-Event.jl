a5c6721a2a63cefb46865727a03124c298a46d1a
tmptrash@mail.ru <k2rwO(H@>
2015-11-08 23:27:47 +0200

    * Small fixes of tests for Event module

--- test/test-Event.jl
+++ test/test-Event.jl
@@ -6,7 +6,7 @@ module TestEvent
     @fact length(Event.create().events) --> 0
   end
 
-  facts("Binding events should work") do
+  facts("Simple event binding should work") do
     flag = false
     obs  = Event.create()
     function handler() flag = true end
@@ -17,15 +17,35 @@ module TestEvent
     @fact flag --> true
   end
 
-  facts("Unbinding events should work") do
-    flag = false
-    obs  = Event.create()
-    function handler() flag = true end
+  facts("Multiple event binding should work") do
+    flag1 = false
+    flag2 = false
+    obs   = Event.create()
+    function handler1() flag1 = true end
+    function handler2() flag2 = true end
 
-    Event.on(obs, "event", handler)
-    Event.off(obs, "event", handler)
+    Event.on(obs, "event", handler1)
+    Event.on(obs, "event", handler2)
     Event.fire(obs, "event")
 
-    @fact flag --> false
+    @fact flag1 --> true
+    @fact flag2 --> true
+  end
+
+  facts("Unbinding events should work") do
+    flag1 = false
+    flag2 = false
+    obs   = Event.create()
+    function handler1() flag1 = true end
+    function handler2() flag2 = true end
+
+    Event.on(obs, "event", handler1)
+    Event.on(obs, "event", handler2)
+    Event.off(obs, "event", handler1)
+    Event.off(obs, "event", handler2)
+    Event.fire(obs, "event")
+
+    @fact flag1 --> false
+    @fact flag2 --> false
   end
 end
