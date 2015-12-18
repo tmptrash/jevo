d93400c656889c79bd631ef8468dc1eb8deb6a76
DeadbraiN <tmptrash@mail.ru>
2015-12-19 01:29:31 +0200

    structure changes

---
+++ run-tests.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+julia --color=yes test/run-tests.jl
--- test/run-tests.jl
+++ test/run-tests.jl
@@ -14,7 +14,7 @@ test_regex = r"^test-.*\.jl$"
 # This line is needed after migrating to julia v0.4, because
 # include() doesn't search files(modules) in current folder.
 #
-push!(LOAD_PATH, pwd())
+push!(LOAD_PATH, "$(pwd())/src")
 
 #
 # All test files should be in "test" folder
