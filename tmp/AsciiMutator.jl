2fd41d1dccc39be8ee410553f482314fa38b1784
tmptrash@mail.ru <k2rwO(H@>
2015-12-22 00:53:59 +0200

    + Added test script with string based mutations

---
+++ tmp/AsciiMutator.jl
@@ -0,0 +1,35 @@
+module AsciiMutator
+  using Helper
+
+  export mutate
+  export Code
+
+  type Code
+    #
+    # Start string should contain 3 symbols: unchanged 1, empty 2 and unchanged 3.
+    # e.g.: "; ;"
+    #
+    str::ASCIIString
+    function Code() new("; ;") end
+  end
+
+  function mutate(code::Code, prob::Array{Int})
+    p = Helper.getProbIndex(prob)
+    l = length(code.str) - 1
+    i = rand(2:l)
+    c = Char(rand(32:126))
+
+    # insert
+    if p === 1
+      code.str = string(code.str[1:i-1], c, code.str[i:end])
+    # change
+    elseif p === 2
+      code.str = string(code.str[1:i-1], c, code.str[i+1:end])
+    # delete
+    elseif l > 2
+      code.str = string(code.str[1:i-1], code.str[i+1:end])
+    end
+
+    code
+  end
+end
