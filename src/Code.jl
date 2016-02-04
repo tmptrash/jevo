e7a92d4b134e623d0022e858fd7d42b5b6e3bc21
tmptrash@mail.ru <k2rwO(H@>
2016-02-05 01:21:05 +0200

    * sync

--- src/Code.jl
+++ src/Code.jl
@@ -74,12 +74,12 @@ module Code
     if v1 === :nothing return Expr(:nothing) end
 
     if typ === ASCIIString 
-      return :($(v1) = $(v2) * $(v3))
+      return :($v1 = $(v2) * $v3)
     elseif typ === Bool
-      return :($(v1) = $(v2) & $(v3))
+      return :($v1 = $v2 & $v3)
     end
 
-    :($(v1) = $(v2) + $(v3))
+    :($v1 = $v2 + $v3)
   end
   #
   # @cmd
@@ -106,18 +106,18 @@ module Code
     # "qwerty" - "111" = "qwe"
     #
     if typ === ASCIIString
-      return :($(v1) = $(v2)[1:(length(v3) > length(v2) > 0 ? 0 : length(v2) - length(v3))])
+      return :($(v1) = $(v2)[1:(length($v3) > length($v2) > 0 ? 0 : length($v2) - length($v3))])
     #
     # true  - true  = false, true  - false = true, 
     # false - false = false, false - true  = true
     #
     elseif typ === Bool
-      return :($(v1) = Bool(abs($(v2) - $(v3))))
+      return :($v1 = Bool(abs($v2 - $v3)))
     end
     #
     # Numeric types are here
     #
-    :($(v1) = $(v2) - $(v3))
+    :($v1 = $v2 - $v3)
   end
   #
   # @cmd
@@ -139,7 +139,7 @@ module Code
 
     if v1 === :nothing return Expr(:nothing) end
 
-    :($(v1) = $(v2) * $(v3))
+    :($v1 = $v2 * $v3)
   end
   #
   # @cmd
@@ -165,12 +165,42 @@ module Code
     # "qwerty" / "111" = "qw"
     #
     if typ === ASCIIString
-      return :($(v1) = $(v2)[1:(length(v3) > length(v2) > 0 ? 0 : div(length(v2), length(v3)))])
+      return :($v1 = $v2[1:(length($v3) > length($v2) > 0 ? 0 : div(length($v2), length($v3)))])
     elseif typ === Bool
       return :($(v1) = $(v2) | $(v3))
     end
 
     :($(v1) = $(v2) / $(v3))
+  end
+  #
+  # @cmd
+  # Calculates reminder of division of two numbers. For ASCIIString
+  # calculates reminder of cutting: "12345" % "23" = "345". It uses
+  # length of second string for cut. For Bool uses | operator.
+  # @param org Organism we have to mutate
+  # @param fn Parent(current) function unique name
+  # we are working in
+  # @param block Current flock within fn function
+  # @return {Expr}
+  #
+  function reminder(org::Creature.Organism, fn::ASCIIString, block::Expr)
+    local typ::DataType = @getType()
+    local v1::Symbol    = @getVar(org, fn, typ)
+    local v2::Symbol    = @getVar(org, fn, typ)
+    local v3::Symbol    = @getVar(org, fn, typ)
+
+    if v1 === :nothing return Expr(:nothing) end
+    #
+    # "1234"   / "854" = "1"   (just cut v1 by length of v1 / v2)
+    # "qwerty" / "111" = "qw"
+    #
+    if typ === ASCIIString
+      return :($v1 = $v2[(length($v3) > length($v2) ? 1 : length($v3)):(length($v3) > length($v2) > 0 ? 0 : end)])
+    elseif typ === Bool
+      return :($v1 = $v2 | $v3)
+    end
+
+    :($v1 = $v2 / $v3)
   end
   #
   # @cmd
