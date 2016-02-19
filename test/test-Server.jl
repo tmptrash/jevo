a50eeb9b067ddc0b48de334ef5a55b7bc515a44d
DeadbraiN <tmptrash@mail.ru>
2016-02-20 01:39:06 +0200

    * sync

--- test/test-Server.jl
+++ test/test-Server.jl
@@ -10,7 +10,6 @@
 # TODO: - test pooling
 # TODO: - test pooling from many clients
 # TODO: - test sending request after client stop
-# TODO: - test servers recriation on the same ip/post. port shouldn't be used
 #
 module TestServer
   using FactCheck
@@ -28,10 +27,12 @@ module TestServer
     con = Server.create(IP, PORT)
     Server.run(con)
     Server.stop(con)
+    yield()
   end
   facts("Tests server creation and stop without run") do
     con = Server.create(IP, PORT)
     Server.stop(con)
+    yield()
   end
   facts("Tests two servers on the same port creation") do
     con1 = Server.create(IP, PORT)
@@ -42,6 +43,7 @@ module TestServer
 
     Server.stop(con2)
     Server.stop(con1)
+    yield()
   end
   facts("Tests stopping the server and create again") do
     con = Server.create(IP, PORT)
@@ -51,6 +53,7 @@ module TestServer
     con = Server.create(IP, PORT)
     @fact Server.isOk(con) --> true
     Server.stop(con)
+    yield()
   end
   facts("Tests server receiving command") do
     #
@@ -76,5 +79,38 @@ module TestServer
 
     Client.stop(ccon)
     Server.stop(scon)
+    yield()
+  end
+  facts("Tests server receiving commands from two clients") do
+    #
+    # server and client creation
+    #
+    local answer::Array{Command,1} = Command[]
+    local scon::ServerConnection = Server.create(IP, PORT)
+    local ccon1::ClientConnection = Client.create(IP, PORT)
+    local ccon2::ClientConnection = Client.create(IP, PORT)
+    local i::Int = 0
+
+    Event.on(scon.observer, Server.EVENT_COMMAND, (cmd, ans)->push!(answer, Command(cmd.fn, deepcopy(cmd.args))))
+    Server.run(scon)
+    Client.request(ccon1, 1, 10) # function - 1, parameter - 10
+    Client.request(ccon2, 2, 20) # function - 2, parameter - 20
+    #
+    # This line is very important. yield() runs the interconnection 
+    # loop between client and server.
+    #
+    Timer((t)->(yield(); if (i+=1) > TIMEOUT * 2 || answer !== nothing close(t) end), 0, 1)
+    @fact i <= TIMEOUT * 2 --> true
+    @fact length(answer) --> 2
+    @fact (answer[1].fn === 1 || answer[1].fn === 2) --> true
+    @fact (answer[1].args[1] === 10 || answer[1].args[1] === 20) --> true
+    @fact (answer[2].fn === 1 || answer[2].fn === 2) --> true
+    @fact (answer[2].args[1] === 10 || answer[2].args[1] === 20) --> true
+    yield()
+
+    Client.stop(ccon2)
+    Client.stop(ccon1)
+    Server.stop(scon)
+    yield()
   end
 end
