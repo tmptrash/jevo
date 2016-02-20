067b2d7cb8c281301909a113cd994f7740f27aa0
DeadbraiN <tmptrash@mail.ru>
2016-02-20 17:51:09 +0200

    * Tests for Server module

--- test/test-Server.jl
+++ test/test-Server.jl
@@ -1,8 +1,5 @@
 #
 # TODO: add tests:
-# TODO: - server+client start, client stop, create other client
-# TODO: - server start, create two clients
-# TODO: - server start, create two clients, send two messages from two clients
 # TODO: - server start, client start, send big data in both directions
 # TODO: - client start without server
 # TODO: - start two servers on same port/ip and one client, send data to server, should work only first one
@@ -21,6 +18,15 @@ module TestServer
   const IP      = ip"127.0.0.1"
   const PORT    = Config.val(:CONNECTION_SERVER_PORT)
   const TIMEOUT = 30
+
+  #
+  # Waits for timeout or function fn() to return true
+  #
+  function wait(fn::Function)
+    i = 0
+    t = Timer((t)->(yield(); if (i+=1) > TIMEOUT || fn() close(t) end), 0, 1)
+    while isopen(t) yield() end
+  end
 
   facts("Tests server creation and start") do
     con = Server.create(IP, PORT)
@@ -57,17 +63,11 @@ module TestServer
     local answer::Command        = Command(0, Array{Any}())
     local scon::ServerConnection = Server.create(IP, PORT)
     local ccon::ClientConnection = Client.create(IP, PORT)
-    local i::Int                 = 0
 
     Event.on(scon.observer, Server.EVENT_COMMAND, (cmd, ans)->answer = Command(cmd.fn, deepcopy(cmd.args)))
     Server.run(scon)
     Client.request(ccon, 1, 10) # function - 1, parameter - 10
-    #
-    # This line is very important. yield() runs the interconnection 
-    # loop between client and server.
-    #
-    Timer((t)->(yield(); if (i+=1) > TIMEOUT || answer.fn !== 0 close(t) end), 0, 1)
-    @fact i <= TIMEOUT   --> true
+    wait(()->answer.fn !== 0)
     @fact answer.fn      --> 1
     @fact answer.args[1] --> 10
 
@@ -75,14 +75,10 @@ module TestServer
     Server.stop(scon)
   end
   facts("Tests server receiving commands from two clients") do
-    #
-    # server and client creation
-    #
     local answer::Array{Command,1} = Command[]
     local scon::ServerConnection = Server.create(IP, PORT)
     local ccon1::ClientConnection = Client.create(IP, PORT)
     local ccon2::ClientConnection = Client.create(IP, PORT)
-    local i::Int = 0
 
     Event.on(scon.observer, Server.EVENT_COMMAND, (cmd, ans)->push!(answer, Command(cmd.fn, deepcopy(cmd.args))))
     Server.run(scon)
@@ -92,8 +88,7 @@ module TestServer
     # This line is very important. yield() runs the interconnection 
     # loop between client and server.
     #
-    Timer((t)->(yield(); if (i+=1) > TIMEOUT * 2 || answer !== nothing close(t) end), 0, 1)
-    @fact i <= TIMEOUT * 2 --> true
+    wait(()->length(answer) > 0)
     @fact length(answer) --> 2
     @fact (answer[1].fn === 1 || answer[1].fn === 2) --> true
     @fact (answer[1].args[1] === 10 || answer[1].args[1] === 20) --> true
@@ -105,13 +100,9 @@ module TestServer
     Server.stop(scon)
   end
   facts("Tests sending the message to stopped server") do
-    #
-    # server and client creation
-    #
     local answer::Command        = Command(0, Array{Any}())
     local scon::ServerConnection = Server.create(IP, PORT)
     local ccon::ClientConnection = Client.create(IP, PORT)
-    local i::Int                 = 0
 
     Event.on(scon.observer, Server.EVENT_COMMAND, (cmd, ans)->answer = Command(cmd.fn, deepcopy(cmd.args)))
     Server.run(scon)
@@ -119,11 +110,47 @@ module TestServer
     Server.stop(scon)
     Client.request(ccon, 2, 20) # function - 2, parameter - 20
 
-    i = 0; Timer((t)->(yield(); if (i+=1) > TIMEOUT || !Server.isOk(scon) close(t) end), 0, 1)
-    @fact i <= TIMEOUT   --> true
+    wait(()->!Server.isOk(scon))
     @fact answer.fn      --> 1
     @fact answer.args[1] --> 10
 
     Client.stop(ccon)
+  end
+  facts("Tests client recreation and send a message") do
+    local answer::Command        = Command(0, Array{Any}())
+    local scon::ServerConnection = Server.create(IP, PORT)
+    local ccon::ClientConnection = Client.create(IP, PORT)
+
+    Event.on(scon.observer, Server.EVENT_COMMAND, (cmd, ans)->answer = Command(cmd.fn, deepcopy(cmd.args)))
+    Server.run(scon)
+    Client.request(ccon, 1, 10)
+    Client.stop(ccon)
+    ccon = Client.create(IP, PORT)
+    Client.request(ccon, 2, 20)
+    wait(()->answer.fn === 2)
+
+    @fact answer.fn      --> 2
+    @fact answer.args[1] --> 20
+
+    Client.stop(ccon)
+    Server.stop(scon)
+  end
+  facts("Tests big data send") do
+    local answer::Command        = Command(0, Array{Any}())
+    local scon::ServerConnection = Server.create(IP, PORT)
+    local ccon::ClientConnection = Client.create(IP, PORT)
+    local i::Int                 = 0
+    local data::Array{Int, 1}    = zeros(Int, 100000)
+
+    Event.on(scon.observer, Server.EVENT_COMMAND, (cmd, ans)->answer = Command(cmd.fn, deepcopy(cmd.args)))
+    Server.run(scon)
+    Client.request(ccon, 1, data)
+    wait(()->answer.fn !== 0)
+
+    @fact answer.fn --> 1
+    @fact answer.args[1] == data --> true
+
+    Client.stop(ccon)
+    Server.stop(scon)
   end
 end
