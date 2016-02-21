6b1f7ca03b852adade3f2f5350330ac94d74223e
DeadbraiN <tmptrash@mail.ru>
2016-02-21 16:45:38 +0200

    * Changed CI script to build the application only from git repo, but not from local copy.

--- build/ci.bat
+++ build/ci.bat
@@ -4,10 +4,16 @@ rem This is mega cool Continue Integration script :)
 rem It runs our unit tests all the time with 1 min delay
 rem In case of tests errors, it stops and shows errors
 rem
+cd jevo
+if %ERRORLEVEL% NEQ 0 goto end
 :loop
+    git pull .
+	if %ERRORLEVEL% NEQ 0 goto end
+	cd build
     cmd /c run-tests-ci.bat
 	if %ERRORLEVEL% NEQ 0 goto end
-    sleep 120
+	cd ..
+    sleep 60
 goto loop
 :end
 cscript build-failed.vbs "The build is failed!"
--- build/install.bat
+++ build/install.bat
@@ -4,3 +4,10 @@ rem This script should be run first, before
 rem all other...
 rem
 julia --color=yes Dependencies.jl
+rem
+rem This part of install script, clones our
+rem jevo repo to the local machine. We do it
+rem for building application by ci.(bat|sh)
+rem script.
+rem
+git clone https://github.com/tmptrash/jevo.git
