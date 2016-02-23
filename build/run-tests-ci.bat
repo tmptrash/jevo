d0dbb3b9d140e8f7f3a04275c6f2f51080684b5a
DeadbraiN <tmptrash@mail.ru>
2016-02-23 10:08:55 +0200

    * test

--- build/run-tests-ci.bat
+++ build/run-tests-ci.bat
@@ -4,5 +4,5 @@ rem This script is used in pair with ci.bat.
 rem It runs unit tests in separate process
 rem and returns error code or 0 if okay.
 rem
-cmd /c run-tests.bat
+cmd /c build\run-tests.bat
 exit /b %ERRORLEVEL%
