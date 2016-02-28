c9bf4953275377d4758845ea16ef6aa3dc7e42c8
DeadbraiN <tmptrash@mail.ru>
2016-02-28 15:40:28 +0200

    * sync

--- build/run-tests.bat
+++ build/run-tests.bat
@@ -1,7 +1,14 @@
 @echo off
 rem
 rem All test files should be in "test" folder.
-rem The same rule for run-tests.jl
+rem The same rule for run-tests.jl. This script
+rem should be run from root folder.
 rem
+if not exist test (
+  echo ERROR: Wrong run folder! Run this script from root folder.
+  exit /b 1
+)
+echo %ERRORLEVEL%
+verify >nul
 julia --color=yes test\run-tests.jl
 exit /b %ERRORLEVEL%
