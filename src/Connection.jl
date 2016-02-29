cf238456d54dd829228fc4f8aa2092a6bb52f4d2
DeadbraiN <tmptrash@mail.ru>
2016-03-01 00:29:27 +0200

    + Added comments

--- src/Connection.jl
+++ src/Connection.jl
@@ -1,20 +1,26 @@
 #
-# TODO: 
+# Client/Server common declarations. Is used in Client, Server
+# and parent code.
 # 
 # @author DeadbraiN
 #
 module Connection
-  import Event
-  
   export Answer
   export Command
 
-  # TODO:
+  #
+  # This command is used for sending some data from client
+  # to server. It contains from function (or command id) and
+  # arguments. An answer will back in Answer type.
+  #
   type Command
     fn::Integer
     args::Array{Any}
   end
-  # TODO:
+  #
+  # This typeis used for answering from Server to Clients.
+  # data property contains Server response value.
+  #
   type Answer
     data::Any
   end
