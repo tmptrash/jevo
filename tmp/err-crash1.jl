6ba4a6a52a791e62ac13dd9c6f980d080de9c606
DeadbraiN <tmptrash@mail.ru>
2016-03-23 18:25:48 +0200

    * sync

--- tmp/err-crash1.jl
+++ tmp/err-crash1.jl
@@ -16,10 +16,11 @@ function run()
 
   while true
     for i=1:len
+      Helper.save(orgs[i], "code-before.jevo")
       Mutator.mutate(orgs[i])
       orgs[i].codeFn = eval(orgs[i].code)
       try
-	    Helper.save(orgs[i], "code.jevo")
+        Helper.save(orgs[i], "code-after.jevo")
         orgs[i].codeFn(orgs[i])
       catch e
       end
