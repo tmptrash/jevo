e98fd3867ee7814f94ff4d7deec24ee04d384d19
DeadbraiN <tmptrash@mail.ru>
2016-03-29 23:26:22 +0300

    * Working on shorthands for remote terminal

---
+++ src/AppTerminal.jl
@@ -0,0 +1,38 @@
+#
+# This is remote terminal application. It's used for remote
+# configuring and statistics collecting of server. It supports
+# all commands, which are declared in RpcApi.jl module.
+#
+# @author DeadbraiN
+#
+include("ImportFolders.jl")
+
+import Client
+import Creature
+import Connection
+import Config
+using Terminal
+#
+# Answer function. It's called every time on request from server
+# @param ans Server answer
+#
+function onAnswer(ans::Connection.Answer)
+  global answer = ans.data
+  println(answer)
+end
+#
+# Application entry point function. Adds listeners for
+# server responses and initializes Terinal module.
+#
+function main()
+  # TODO: ip should be get from config
+  local con = Client.create(ip"127.0.0.1", Config.val(:CONNECTION_SERVER_PORT))
+
+  if !Client.isOk(con) quit() end
+  Event.on(con.observer, Client.EVENT_ANSWER, onAnswer)
+  Terminal.init(con)
+end
+#
+# App start point
+#
+main()
---
+++ src/net/Terminal.jl
@@ -0,0 +1,62 @@
+#
+# Helper module for Remote Terminal application. It
+# contains shorthands (macroses) for more simple 
+# remote server configuring and requests. 
+#
+# @author DeadbraiN
+#
+module Terminal
+  import RpcApi
+  import Config
+  import Client
+  import Connection
+  import Creature
+
+  export init
+  export termGetRegion
+
+  # const RPC_CREATE_ORGANISMS  = 2
+  # const RPC_CREATE_ORGANISM   = 3
+  # const RPC_SET_CONFIG        = 4
+  # const RPC_GET_CONFIG        = 5
+  # const RPC_MUTATE            = 6
+  # const RPC_GET_IPS           = 7
+  # const RPC_GET_ORGANISM      = 8
+  # const RPC_GET_AMOUNT        = 9
+  # const RPC_GET_ORGANISMS     = 10
+  # const RPC_SET_ENERGY        = 11
+  # const RPC_SET_ENERGY_RND    = 12
+  # const RPC_BACKUP            = 13
+  # const RPC_GET_STATISTICS    = 14
+  # const RPC_DEBUG_GC          = 15
+  #
+  # Contains connection object for shorthands
+  #
+  type TerminalData
+  	con::Client.ClientConnection
+  	TerminalData() = new()
+  end
+  #
+  # This function should be called first before any macros
+  # will be called. It sets connection object as a global.
+  # @param con Connection object
+  #
+  function init(con::Client.ClientConnection)
+  	_termData.con = con
+  end
+  #
+  # Shorthand for RpcApi.RPC_GET_REGION
+  # @param x Start X coordinate of region
+  # @param y Start Y coordinate of region
+  # @param x1 End x. 0 means all width
+  # @param y1 End y. 0 means all height
+  #
+  function termGetRegion(x::Int = 1, y::Int = 1, x1::Int = 0, y1::Int = 0)
+  	Client.request(_termData.con, RpcApi.RPC_GET_REGION, x, y, x1, y1)
+  end
+  #
+  # This object is created only for passing connection
+  # data to macroses.
+  #
+  global _termData = TerminalData()
+end
