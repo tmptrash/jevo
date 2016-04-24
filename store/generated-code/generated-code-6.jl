de22c2e9a0603c68273f70e4dae0a05aee36a18c
DeadbraiN <tmptrash@mail.ru>
2016-04-24 20:04:42 +0300

    * sync

---
+++ store/generated-code/generated-code-6.jl
@@ -0,0 +1,119 @@
+ia> [RpcApi.SimpleOrganism(0x000000000000464e,:(function (o::Creature.Organism,)
+     function func_4(var_2::Int64=8688889982731189829,var_3::Bool=true)
+         Creature.stepUp(o)
+         return var_2
+     end
+     local var_1::Int8 = 48
+     if var_1 == var_1
+     end
+     Creature.stepUp(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepRight(o)
+     Creature.eatRight(o,Int(var_1))
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepLeft(o)
+     var_1 = var_1 - var_1
+     return true
+ end),[100,300,95,0,1,1,1],1,5000,1,25952,0x0104b17d,Dict{Int16,Int16}(),[156,113],12),RpcApi.SimpleOrganism(0x0000000000004656,:(function (o::Creature.Organism,)
+     function func_4(var_2::Int64=8688889982731189829,var_3::Bool=true)
+         return var_2
+     end
+     local var_1::Int8 = 48
+     if var_1 == var_1
+     end
+     Creature.stepUp(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepRight(o)
+     Creature.eatRight(o,Int(var_1))
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepLeft(o)
+     var_1 = var_1 - var_1
+     return true
+ end),[100,300,95,0,1,1,1],1,5000,1,25373,0x0104e81a,Dict{Int16,Int16}(),[137,143],11),RpcApi.SimpleOrganism(0x000000000000455a,:(function (o::Creature.Organism,)
+     function func_4(var_2::Int64=8688889982731189829,var_3::Bool=true)
+         Creature.stepUp(o)
+         return var_2
+     end
+     local var_1::Int8 = 48
+     if var_1 == var_1
+     end
+     Creature.stepUp(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepRight(o)
+     Creature.eatRight(o,Int(var_1))
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepLeft(o)
+     var_1 = var_1 - var_1
+     return true
+ end),[100,300,95,0,1,1,1],1,5000,1,16782,0x0104b17d,Dict{Int16,Int16}(),[125,88],12),RpcApi.SimpleOrganism(0x000000000000468c,:(function (o::Creature.Organism,)
+     function func_4(var_2::Int64=8688889982731189829,var_3::Bool=true)
+         Creature.stepUp(o)
+         Creature.stepLeft(o)
+         return var_2
+     end
+     local var_1::Int8 = 48
+     if var_1 == var_1
+     end
+     Creature.stepUp(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepRight(o)
+     Creature.eatRight(o,Int(var_1))
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepLeft(o)
+     var_1 = var_1 - var_1
+     return true
+ end),[100,300,95,0,1,1,1],1,5000,1,15459,0x0104e81a,Dict{Int16,Int16}(),[53,89],13),RpcApi.SimpleOrganism(0x0000000000004671,:(function (o::Creature.Organism,)
+     function func_4(var_2::Int64=8688889982731189829,var_3::Bool=true)
+         Creature.stepUp(o)
+         return var_2
+     end
+     local var_1::Int8 = 48
+     if var_1 == var_1
+     end
+     Creature.stepUp(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepRight(o)
+     Creature.eatRight(o,Int(var_1))
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepLeft(o)
+     var_1 = var_1 - var_1
+     return true
+ end),[100,300,95,0,1,1,1],1,5000,1,14872,0x0104b17d,Dict{Int16,Int16}(),[40,150],12),RpcApi.SimpleOrganism(0x0000000000004685,:(function (o::Creature.Organism,)
+     function func_4(var_2::Int64=8688889982731189829,var_3::Bool=true)
+         Creature.stepUp(o)
+         return var_2
+     end
+     local var_1::Int8 = 48
+     if var_1 == var_1
+     end
+     Creature.stepUp(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepRight(o)
+     Creature.eatRight(o,Int(var_1))
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepLeft(o)
+     var_1 = var_1 - var_1
+     return true
+ end),[100,300,95,0,1,1,1],1,5000,1,13442,0x0104b17d,Dict{Int16,Int16}(),[100,47],12),RpcApi.SimpleOrganism(0x00000000000045ed,:(function (o::Creature.Organism,)
+     function func_4(var_2::Int64=8688889982731189829,var_3::Bool=true)
+         return var_2
+     end
+     local var_1::Int8 = 48
+     Creature.eatDown(o,Int(var_1))
+     if var_1 == var_1
+     end
+     Creature.stepUp(o)
+     Creature.stepRight(o)
+     Creature.eatRight(o,Int(var_1))
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     Creature.stepLeft(o)
+     var_1 = var_1 - var_1
+     return true
+ end),[100,300,95,0,1,1,1],1,5000,1,12429,0x0104e81a,Dict{Int16,Int16}(),[88,143],11)]
