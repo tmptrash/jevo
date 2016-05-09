99dcd5aa4fbafd149d054d77fe465fae98bab0c4
DeadbraiN <tmptrash@mail.ru>
2016-05-09 14:29:35 +0300

    + Added generated code

---
+++ store/generated-code/generated-code-8.jl
@@ -0,0 +1,556 @@
+ia> [RpcApi.SimpleOrganism(0x00000000000427ea,:(function (o::Creature.Organism,)
+     function func_19(var_17::ASCIIString="A06aRuJT",var_18::Int64=-4494005246402814635)
+         local var_27::Bool = true
+         if var_17 !== var_17
+         end
+         return var_17
+     end
+     local var_1::Int8 = 121
+     for i = 1:var_1
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+     end
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     for i = 1:var_1
+         local var_15::Bool = true
+         local var_9::Bool = true
+         local var_8::Int16 = 23832
+         local var_6::Int8 = 66
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepRight(o)
+         var_9 = var_9 | var_9
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepLeft(o)
+         Creature.eatLeft(o,Int(var_6))
+         var_8 = if haskey(o.mem,var_8)
+                 o.mem[var_8]
+             else
+                 var_8
+             end
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+         Creature.eatDown(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatUp(o,Int(var_6))
+     end
+     Creature.stepUp(o)
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+         Creature.stepLeft(o)
+     end
+     Creature.eatDown(o,Int(var_1))
+     Creature.stepLeft(o)
+     Creature.stepLeft(o)
+     for i = 1:var_1
+         local var_25::Bool = true
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     return true
+ end),[100,300,95,0,1,1,1],1,8534,7,4137644,0x0117ea60,Dict(23832=>23832),[181,326],44),RpcApi.SimpleOrganism(0x0000000000042846,:(function (o::Creature.Organism,)
+     function func_19(var_17::ASCIIString="A06aRuJT",var_18::Int64=-4494005246402814635)
+         return var_17
+     end
+     local var_1::Int8 = 121
+     for i = 1:var_1
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+     end
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     for i = 1:var_1
+         local var_15::Bool = true
+         local var_9::Bool = true
+         local var_8::Int16 = 23832
+         local var_6::Int8 = 66
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepRight(o)
+         var_9 = var_9 | var_9
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepLeft(o)
+         var_8 = if haskey(o.mem,var_8)
+                 o.mem[var_8]
+             else
+                 var_8
+             end
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+         Creature.eatDown(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatUp(o,Int(var_6))
+     end
+     Creature.stepUp(o)
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+         Creature.stepLeft(o)
+     end
+     Creature.eatDown(o,Int(var_1))
+     Creature.stepLeft(o)
+     Creature.stepLeft(o)
+     for i = 1:var_1
+         local var_25::Bool = true
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     return true
+ end),[100,300,95,0,1,1,1],1,8534,1,3555267,0x0117d488,Dict(23832=>23832),[290,328],41),RpcApi.SimpleOrganism(0x00000000000426f1,:(function (o::Creature.Organism,)
+     function func_19(var_17::ASCIIString="A06aRuJT",var_18::Int64=-4494005246402814635)
+         return var_17
+     end
+     local var_1::Int8 = 121
+     for i = 1:var_1
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+     end
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     for i = 1:var_1
+         local var_15::Bool = true
+         local var_9::Bool = true
+         local var_8::Int16 = 23832
+         local var_6::Int8 = 66
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepRight(o)
+         var_9 = var_9 | var_9
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepLeft(o)
+         var_8 = if haskey(o.mem,var_8)
+                 o.mem[var_8]
+             else
+                 var_8
+             end
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+         Creature.eatDown(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatUp(o,Int(var_6))
+     end
+     Creature.stepUp(o)
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+         Creature.stepLeft(o)
+     end
+     Creature.eatDown(o,Int(var_1))
+     Creature.stepLeft(o)
+     Creature.stepLeft(o)
+     for i = 1:var_1
+         local var_25::Bool = true
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     return true
+ end),[100,300,95,0,1,1,1],1,8534,1,3508032,0x0117d488,Dict(23832=>23832),[289,328],41),RpcApi.SimpleOrganism(0x000000000004263e,:(function (o::Creature.Organism,)
+     function func_19(var_17::ASCIIString="A06aRuJT",var_18::Int64=-4494005246402814635)
+         local var_27::Bool = true
+         return var_17
+     end
+     local var_1::Int8 = 121
+     for i = 1:var_1
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+     end
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     for i = 1:var_1
+         local var_15::Bool = true
+         local var_9::Bool = true
+         local var_8::Int16 = 23832
+         local var_6::Int8 = 66
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepRight(o)
+         var_9 = var_9 | var_9
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepLeft(o)
+         var_8 = if haskey(o.mem,var_8)
+                 o.mem[var_8]
+             else
+                 var_8
+             end
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+         Creature.eatDown(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatUp(o,Int(var_6))
+     end
+     Creature.stepUp(o)
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+         Creature.stepLeft(o)
+     end
+     Creature.eatDown(o,Int(var_1))
+     Creature.stepLeft(o)
+     Creature.stepLeft(o)
+     for i = 1:var_1
+         local var_25::Bool = true
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     return true
+ end),[100,300,95,0,1,1,1],1,8534,1,2998839,0x0117beb0,Dict(23832=>23832),[219,333],42),RpcApi.SimpleOrganism(0x0000000000042825,:(function (o::Creature.Organism,)
+     function func_19(var_17::ASCIIString="A06aRuJT",var_18::Int64=-4494005246402814635)
+         return var_17
+     end
+     local var_1::Int8 = 121
+     for i = 1:var_1
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+     end
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     for i = 1:var_1
+         local var_15::Bool = true
+         local var_9::Bool = true
+         local var_8::Int16 = 23832
+         local var_6::Int8 = 66
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepRight(o)
+         var_9 = var_9 | var_9
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepLeft(o)
+         var_8 = if haskey(o.mem,var_8)
+                 o.mem[var_8]
+             else
+                 var_8
+             end
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+         Creature.eatDown(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatUp(o,Int(var_6))
+     end
+     Creature.stepUp(o)
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+         Creature.stepLeft(o)
+     end
+     Creature.eatDown(o,Int(var_1))
+     Creature.stepLeft(o)
+     Creature.stepLeft(o)
+     for i = 1:var_1
+         local var_25::Bool = true
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     return true
+ end),[100,300,95,0,1,1,1],1,8534,1,2978922,0x0117d488,Dict(23832=>23832),[170,330],41),RpcApi.SimpleOrganism(0x00000000000426e9,:(function (o::Creature.Organism,)
+     function func_19(var_17::ASCIIString="A06aRuJT",var_18::Int64=-4494005246402814635)
+         return var_17
+     end
+     local var_1::Int8 = 121
+     for i = 1:var_1
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+     end
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     for i = 1:var_1
+         local var_15::Bool = true
+         local var_9::Bool = true
+         local var_8::Int16 = 23832
+         local var_6::Int8 = 66
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepRight(o)
+         var_9 = var_9 | var_9
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepLeft(o)
+         var_8 = if haskey(o.mem,var_8)
+                 o.mem[var_8]
+             else
+                 var_8
+             end
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+         Creature.eatDown(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatUp(o,Int(var_6))
+     end
+     Creature.stepUp(o)
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+         Creature.stepLeft(o)
+     end
+     Creature.eatDown(o,Int(var_1))
+     Creature.stepLeft(o)
+     Creature.stepLeft(o)
+     for i = 1:var_1
+         local var_25::Bool = true
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     return true
+ end),[100,300,95,0,1,1,1],1,8534,1,2519819,0x0117d488,Dict(23832=>23832),[454,334],41),RpcApi.SimpleOrganism(0x0000000000042740,:(function (o::Creature.Organism,)
+     function func_19(var_17::ASCIIString="A06aRuJT",var_18::Int64=-4494005246402814635)
+         return var_17
+     end
+     local var_1::Int8 = 121
+     for i = 1:var_1
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+     end
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     for i = 1:var_1
+         local var_15::Bool = true
+         local var_9::Bool = true
+         local var_8::Int16 = 23832
+         local var_6::Int8 = 66
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepRight(o)
+         var_9 = var_9 | var_9
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepLeft(o)
+         var_8 = if haskey(o.mem,var_8)
+                 o.mem[var_8]
+             else
+                 var_8
+             end
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+         Creature.eatDown(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatUp(o,Int(var_6))
+     end
+     Creature.stepUp(o)
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+         Creature.stepLeft(o)
+     end
+     Creature.eatDown(o,Int(var_1))
+     Creature.stepLeft(o)
+     Creature.stepLeft(o)
+     for i = 1:var_1
+         local var_25::Bool = true
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     return true
+ end),[100,300,95,0,1,1,1],1,8534,1,2144983,0x0117d488,Dict(23832=>23832),[445,334],41),RpcApi.SimpleOrganism(0x00000000000427f8,:(function (o::Creature.Organism,)
+     function func_19(var_17::ASCIIString="A06aRuJT",var_18::Int64=-4494005246402814635)
+         return var_17
+     end
+     local var_1::Int8 = 121
+     for i = 1:var_1
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+     end
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     for i = 1:var_1
+         local var_15::Bool = true
+         local var_9::Bool = true
+         local var_8::Int16 = 23832
+         local var_6::Int8 = 66
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepRight(o)
+         var_9 = var_9 | var_9
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepLeft(o)
+         var_8 = if haskey(o.mem,var_8)
+                 o.mem[var_8]
+             else
+                 var_8
+             end
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+         Creature.eatDown(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatUp(o,Int(var_6))
+     end
+     Creature.stepUp(o)
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+         Creature.stepLeft(o)
+     end
+     Creature.eatDown(o,Int(var_1))
+     Creature.stepLeft(o)
+     Creature.stepLeft(o)
+     for i = 1:var_1
+         local var_25::Bool = true
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     return true
+ end),[100,300,95,0,1,1,1],1,8534,1,2084712,0x0117d488,Dict(23832=>23832),[194,332],41),RpcApi.SimpleOrganism(0x0000000000042786,:(function (o::Creature.Organism,)
+     function func_19(var_17::ASCIIString="A06aRuJT",var_18::Int64=-4494005246402814635)
+         return var_17
+     end
+     local var_1::Int8 = 121
+     for i = 1:var_1
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+     end
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     for i = 1:var_1
+         local var_15::Bool = true
+         local var_9::Bool = true
+         local var_8::Int16 = 23832
+         local var_6::Int8 = 66
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepRight(o)
+         var_9 = var_9 | var_9
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepLeft(o)
+         var_8 = if haskey(o.mem,var_8)
+                 o.mem[var_8]
+             else
+                 var_8
+             end
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+         Creature.eatDown(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatUp(o,Int(var_6))
+     end
+     Creature.stepUp(o)
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+         Creature.stepLeft(o)
+     end
+     Creature.eatDown(o,Int(var_1))
+     Creature.stepLeft(o)
+     Creature.stepLeft(o)
+     for i = 1:var_1
+         local var_25::Bool = true
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     return true
+ end),[100,300,95,0,1,1,1],1,8534,1,2052945,0x0117d488,Dict(23832=>23832),[499,322],41),RpcApi.SimpleOrganism(0x0000000000042741,:(function (o::Creature.Organism,)
+     function func_19(var_17::ASCIIString="A06aRuJT",var_18::Int64=-4494005246402814635)
+         return var_17
+     end
+     local var_1::Int8 = 121
+     for i = 1:var_1
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+     end
+     Creature.stepDown(o)
+     Creature.eatUp(o,Int(var_1))
+     for i = 1:var_1
+         local var_15::Bool = true
+         local var_9::Bool = true
+         local var_8::Int16 = 23832
+         local var_6::Int8 = 66
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepRight(o)
+         var_9 = var_9 | var_9
+         Creature.eatRight(o,Int(var_6))
+         Creature.stepLeft(o)
+         var_8 = if haskey(o.mem,var_8)
+                 o.mem[var_8]
+             else
+                 var_8
+             end
+         Creature.eatLeft(o,Int(var_6))
+         Creature.stepRight(o)
+         Creature.stepRight(o)
+         Creature.eatDown(o,Int(var_6))
+         Creature.eatRight(o,Int(var_6))
+         Creature.eatUp(o,Int(var_6))
+     end
+     Creature.stepUp(o)
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+         Creature.stepLeft(o)
+     end
+     Creature.eatDown(o,Int(var_1))
+     Creature.stepLeft(o)
+     Creature.stepLeft(o)
+     for i = 1:var_1
+         local var_25::Bool = true
+     end
+     for i = 1:var_1
+         Creature.stepLeft(o)
+     end
+     return true
+ end),[100,300,95,0,1,1,1],1,8534,1,1980572,0x0117ea60,Dict(23832=>23832),[443,334],41)]
