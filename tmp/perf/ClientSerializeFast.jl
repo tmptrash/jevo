0ffc482a2853c9b63c7f3e1cb43cdf74796f654a
DeadbraiN <tmptrash@mail.ru>
2016-06-11 15:03:39 +0300

    * sync

---
+++ tmp/perf/ClientSerializeFast.jl
@@ -0,0 +1,10 @@
+t = time()
+client = connect(2000)
+for i::Int=1:1000000
+  read(client, UInt8)
+  if i::Int === 1 t::Float64 = time() end
+  if i::Int === 1000000
+    t::Float64 = time() - t::Float64
+    println("rps: ", i::Int / t::Float64, ", time: ", t::Float64)
+  end
+end
---
+++ tmp/perf/ServerSerializeFast.jl
@@ -0,0 +1,11 @@
+#
+# Calculates requests per second from client to server without response, but
+# using only naked sockets.
+# Results: rps: 29303.170665295427, time: 34.12599992752075
+#
+server = listen(2000)
+sock   = accept(server)
+
+function run()
+  for i=1:1000000 write(sock, UInt8(rand(1:100))) end
+end
