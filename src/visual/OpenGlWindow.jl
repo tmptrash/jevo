165a7b37b2746830e2e8e1681114344b7a5bbd18
DeadbraiN <tmptrash@mail.ru>
2016-06-25 00:09:40 +0300

    + Added OpenGlWindow module for fast 2d drawing

---
+++ src/visual/OpenGlWindow.jl
@@ -0,0 +1,113 @@
+#
+# This module creates a platform independent window with a canvas inside.
+# It uses OpenGL and GR libraries for that. Has an ability to draw a color pixels
+# on a canvas. Uses RGB color format for drawing. It implements lazy update
+# logic. It means, that canvas will be updated only after calling update()
+# method. So, you may draw many dots, but user will not see them. Only after
+# calling update() all dots will appear.
+#
+# Issues:
+#
+# Usage:
+#   import OpenGlWindow
+#   ...
+#   win = OpenGlWindow.create(300, 300)
+#   CanvasWindow.dot(win, 20, 20, UInt32(11197883)) # R=AA,G=DD,B=BB
+#   OpenGlWindow.dot(win, 30, 30, UInt32(11197883)) # R=AA,G=DD,B=BB
+#   OpenGlWindow.update(win)                        # Two dots will be shown
+#   ...
+#   OpenGlWindow.destroy(win)
+#
+# @author DeadbraiN
+# TODO: check if this mosule platform independent
+#
+module OpenGlWindow
+  import GR
+  import Colors
+  import Config
+
+  export Window
+  export create
+  export dot
+  export update
+  export title
+  export destroy
+
+  #
+  # Data type, which contains window related data: Window, Canvas, Context,...
+  # Is used in all public methods as a canvas for drawing.
+  #
+  type Window
+    scale::Int
+    dotBuf::Array{Int, 1}
+    width::Int
+    height::Int
+    running::Bool
+  end
+  #
+  # Creates window and returns window related data type, which is used
+  # in all public methods of current module. Sets default background
+  # color according to global configuration.
+  # @param width Window width in pixels
+  # @param height Window height in pixels
+  # @param scale Scale of canvas 1:1 by default
+  # TODO: @param title Window title
+  # @return Window object
+  #
+  function create(width::Int, height::Int, scale::Int = Config.val(:WORLD_SCALE), title::ASCIIString = "")
+    GR.setwindow(1, width * scale, 1, height * scale)
+    GR.setviewport(0, 1, 0, 1)
+    #
+    # Settings for fillrect() function
+    #
+    GR.setfillintstyle(GR.INTSTYLE_SOLID)
+    GR.setlinetype(GR.LINETYPE_SOLID)
+    GR.setlinewidth(1)
+    #
+    # Settings for dots drawing with polymarker() function
+    #
+    GR.setmarkertype(GR.MARKERTYPE_DOT)
+    GR.setmarkersize(1)
+
+    Window(scale, Int[0, 0], width, height, true)
+  end
+  #
+  # Draws one dot (point) on the canvas with specified color
+  # @param win Windows type
+  # @param x X coordinate of the point
+  # @param y Y coordinate of the point
+  # @param color Color of the dot. We use only last three bytes (24bits) of four.
+  #
+  function dot(win::Window, x::Int, y::Int, color::UInt32)
+    if win.scale > 1
+      x = (x - 1) * win.scale + 1
+      y = win.height * win.scale - (y - 1) * win.scale + 1
+      GR.setlinecolorind(Int(color))
+      GR.setfillcolorind(Int(color))
+      GR.fillrect(x, y, x + win.scale, y + win.scale)
+    else
+      setmarkercolorind(Int(color))
+      GR.polymarker(win.dotBuf[1] = x, win.dotBuf[2] = y)
+    end
+  end
+  #
+  # Sets title to specified
+  # @param win Window data type
+  # @param title String title
+  # TODO:
+  function title(win::Window, title::ASCIIString) end
+  #
+  # Updates the canvas. It's not nessesary to update it after
+  # every drawing. It's better to update it after several dots
+  # are drown.
+  # @param win Current window
+  #
+  function update(win::Window) GR.updatews() end
+  #
+  # Destroys OpenGL windows by quitting from main loop
+  # @param win Window object to destroy
+  #
+  function destroy(win::Window)
+    win.running = false
+  end
+end
--- tmp/perf/opengl.jl
+++ tmp/perf/opengl.jl
@@ -1,11 +1,13 @@
 using GR
 
 # GR initialization
-setwindow(-2, 2, -2, 2)
+setwindow(1, 640, 1, 480)
 setviewport(0, 1, 0, 1)
-setfillintstyle(1)
+setfillintstyle(GR.INTSTYLE_SOLID)
 setlinetype(GR.LINETYPE_SOLID)
 setlinewidth(1)
+GR.setmarkertype(GR.MARKERTYPE_DOT)
+GR.setmarkersize(1)
 
 # our approach for calculating ips
 type Rps
@@ -15,35 +17,39 @@ end
 _rps = Rps(0.0, 0)
 
 # rect coordinates
-x1 = -2.0
-x2 = 2.0
-y1 = -2.0
-y2 = 2.0
+x1 = 1.0
+x2 = 640.0
+y1 = 1.0
+y2 = 480.0
 
 setcolorrep(1, 1.0, 1.0, 1.0)
 setcolorrep(2, 0.0, 0.0, 0.0)
 # press ctrl-c to stop the demo
-while true
+i = 0
+while i < 500000 # ~5seconds
+  i += 1
   # calculating ips (Iterations per second)
   if time() - _rps.ts > 1.0
-	_rps.ts = time()
-	print("rps: ", _rps.req)
-	_rps.req = 0
+  	_rps.ts = time()
+  	print("rps: ", _rps.req)
+  	_rps.req = 0
   end
   _rps.req += 1
 
   #clearws()
   # this code clears previous rect
-  setlinecolorind(2)
-  setfillcolorind(2)
-  fillrect(x1, x2, y1, y2)
-  
-  setlinecolorind(1)
-  setfillcolorind(1)
-  x1 = Float64(rand(-2:2))
-  x2 = Float64(rand(-2:2))
-  y1 = Float64(rand(-2:2))
-  y2 = Float64(rand(-2:2))
-  fillrect(x1, x2, y1, y2)
+  #setlinecolorind(2)
+  #setfillcolorind(2)
+  #fillrect(x1, x2, y1, y2)
+
+  #setlinecolorind(1)
+  #setfillcolorind(1)
+  #x1 = Float64(rand(-2:2))
+  #x2 = Float64(rand(-2:2))
+  #y1 = Float64(rand(-2:2))
+  #y2 = Float64(rand(-2:2))
+  #fillrect(x1, x1 + 2, y2, y2 + 2)
+  setmarkercolorind(1000)
+  polymarker([x2], [y2 - y1 + 1])
   updatews()
 end
