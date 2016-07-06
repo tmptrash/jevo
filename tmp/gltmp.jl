512488fa7cd32fbe2182cc494e362e75de5a8e29
DeadbraiN <tmptrash@mail.ru>
2016-07-07 02:53:23 +0300

    + Added stat data

--- tmp/gltmp.jl
+++ tmp/gltmp.jl
@@ -1,36 +1,65 @@
-using GR
-# min supportted resolution is: 351x350 (width should be greated then height)
-w = 763
-h = 459
+#=using GR
 
-# size of the window (34.54x19.43cm)
 mwidth, mheight, width, height = GR.inqdspsize()
-println(GR.inqdspsize())
+w = 400
+h = 300
 if w >= h
-	ratio = Float64(h) / w
-	msize = mwidth * w / width
-	GR.setwsviewport(0, msize, 0, msize * ratio)
-	GR.setwswindow(0, 1, 0, ratio)
+    ratio = float(h) / w
+    msize = mwidth * w / width
+    GR.setwsviewport(0, msize, 0, msize * ratio)
+    GR.setwswindow(0, 1, 0, ratio)
 else
-	ratio = float(w) / h
-	msize = mheight * h / height
-	GR.setwsviewport(0, msize * ratio, 0, msize)
-	GR.setwswindow(0, ratio, 0, 1)
+    ratio = float(w) / h
+    msize = mheight * h / height
+    GR.setwsviewport(0, msize * ratio, 0, msize)
+    GR.setwswindow(0, ratio, 0, 1)
 end
 
 setviewport(0, 1, 0, ratio)
-##setwindow(1, w + 9.5, -12.7, h)
-setwindow(1, w, 1, h)
-
-setcolorrep(2, 0.0, 0.0, 0.0)
-setlinecolorind(2)
-setlinewidth(1)
-setfillintstyle(1)
+setwindow(1, w + 9.5, -12.7, h)
 
 i = 0
 while i < 300000
   #clearws()
   drawrect(1, w, 1, h)
+  polyline([1,w],[1,h])
+  polyline([1,w],[h,1])
   updatews()
   i += 1
+end
+=#
+
+
+using GR
+
+mwidth, mheight, width, height = GR.inqdspsize()
+
+while true
+    clearws()
+
+    w = 400 + round(Int, rand() * 200)
+    h = 400 + round(Int, rand() * 200)
+    if w >= h
+        ratio = float(h) / w
+        msize = mwidth * w / width
+        GR.setwsviewport(0, msize, 0, msize * ratio)
+        GR.setwswindow(0, 1, 0, ratio)
+        setviewport(0, 1, 0, ratio)
+    else
+        ratio = float(w) / h
+        msize = mheight * h / height
+        GR.setwsviewport(0, msize * ratio, 0, msize)
+        GR.setwswindow(0, ratio, 0, 1)
+        setviewport(0, ratio, 0, 1)
+    end
+
+    setwindow(1, w + 9.5, -12.7, h)
+
+    drawrect(1, w, 1, h)
+    polyline([1,w],[1,h])
+    polyline([1,w],[h,1])
+    text(0.05, 0.05, "$w x $h")
+	sleep(1)
+
+    updatews()
 end
