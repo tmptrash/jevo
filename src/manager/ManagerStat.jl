23dda1187bd162cc2402319b6302ce345eb53ca5
DeadbraiN <deadbrainman@gmail.com>
2016-08-26 22:19:56 +0300

    + small refactoring...

--- src/manager/ManagerStat.jl
+++ src/manager/ManagerStat.jl
@@ -16,27 +16,26 @@ function _updateStat(stamp::Float64)
     if length(bestOrgs) > 0
       org  = bestOrgs[1]
       code = string(org.code)
-	  # TODO: remove extra string convertion
       json = string(
         "{",
         string("\"Probabilities\"       : ", org.mutationProbabilities, ",\n"),
         string("\"Amount of functions\" : ", length(split(code, "function ")) - 2, ",\n"), # minus main function
         string("\"Functions calls\"     : ", length(split(code, "func_")) - 1, ",\n"),
-        string("\"Mutations on clone\"  : ", string(org.mutationsOnClone), ",\n"),
-        string("\"Mutations period\"    : ", string(org.mutationPeriod), ",\n"),
-        string("\"Mutations amount\"    : ", string(org.mutationAmount), ",\n"),
-        string("\"Energy\"              : ", string(org.energy), ",\n"),
+        string("\"Mutations on clone\"  : ", org.mutationsOnClone, ",\n"),
+        string("\"Mutations period\"    : ", org.mutationPeriod, ",\n"),
+        string("\"Mutations amount\"    : ", org.mutationAmount, ",\n"),
+        string("\"Energy\"              : ", org.energy, ",\n"),
         string("\"for operators\"       : ", length(split(code, "for i::Int8")) - 1, ",\n"),
         string("\"if operators\"        : ", length(split(code, "if var")) - 1, ",\n"),
         string("\"getEnergy\"           : ", length(split(code, "Creature.getEnergy")) - 1, ",\n"),
         string("\"Memory reading\"      : ", length(split(code, "haskey(o.mem")) - 1, ",\n"),
         string("\"Eating\"              : ", length(split(code, "Creature.eat")) - 1, ",\n"),
         string("\"Moving\"              : ", length(split(code, "Creature.step")) - 1, ",\n"),
-        string("\"Amount of lines\"     : ", string(org.codeSize), ",\n"),
-        string("\"Amount of organisms\" : ", string(length(Manager._data.tasks)), ",\n"),
-        string("\"IPS\"                 : ", string(Config.val(:WORLD_IPS)), ",\n"),
+        string("\"Amount of lines\"     : ", org.codeSize, ",\n"),
+        string("\"Amount of organisms\" : ", length(Manager._data.tasks), ",\n"),
+        string("\"IPS\"                 : ", Config.val(:WORLD_IPS), ",\n"),
         string("\"World energy\"        : ", Manager._getWorldEnergy(), ",\n"),
-        string("\"Generation number\"   : ", string(Manager._data.totalOrganisms), "\n"),
+        string("\"Generation number\"   : ", Manager._data.totalOrganisms, "\n"),
         "},\n"
       )
       _appendToFile(json, "file.json")
