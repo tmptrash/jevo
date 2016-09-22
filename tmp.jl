e8739dcf2ee63611da41ee1af2cbd7143e491ca5
DeadbraiN <deadbrainman@gmail.com>
2016-09-22 19:42:35 +0300

    - Removed deprecated tests

---
+++ tmp.jl
@@ -0,0 +1,369 @@
+function (o::Creature.Organism,)
+    local var_294::Int64 = -3163098685518489454
+    function func_283(var_281::Int64=8693043452684626053,var_282::Int64=8028444728468592344)
+        Creature.stepLeft(o)
+        return var_281
+    end
+    function func_279(var_276::Int16=8208,var_277::Int8=18,var_278::Int16=-29784)
+        local var_341::Int8 = 71
+        Creature.stepDown(o)
+        Creature.eatRight(o,Int(var_277))
+        Creature.eatLeft(o,Int(var_277))
+        Creature.eatRight(o,Int(var_277))
+        var_277 = var_277 / var_277
+        Creature.eatRight(o,Int(var_277))
+        var_276 = if haskey(o.mem,var_276)
+                o.mem[var_276]
+            else
+                var_276
+            end
+        return var_276
+    end
+    local var_242::Int64 = -6996791020495759508
+    function func_210(var_209::Int8=-64)
+        local var_344::Int8 = 100
+        local var_313::Int64 = 6958562909938990501
+        local var_312::String = "suTzmhT0"
+        local var_280::Int16 = 15933
+        Creature.stepDown(o)
+        var_280 = var_280 / var_280
+        var_209 = var_209 - var_209
+        Creature.stepLeft(o)
+        if var_209 > var_209
+            Creature.stepUp(o)
+            Creature.stepRight(o)
+            Creature.stepDown(o)
+            Creature.stepRight(o)
+        end
+        Creature.eatLeft(o,Int(var_209))
+        var_313 = Int(Creature.getEnergy(o,Int(var_280),Int(var_280)))
+        Creature.stepRight(o)
+        Creature.stepDown(o)
+        Creature.eatRight(o,Int(var_209))
+        var_312 = var_312 * var_312
+        if var_280 == var_280
+            Creature.stepLeft(o)
+            Creature.stepRight(o)
+            Creature.stepRight(o)
+            Creature.stepLeft(o)
+            Creature.stepLeft(o)
+        end
+        Creature.stepRight(o)
+        Creature.stepDown(o)
+        Creature.eatDown(o,Int(var_209))
+        Creature.stepRight(o)
+        var_280 = if haskey(o.mem,var_280)
+                o.mem[var_280]
+            else
+                var_280
+            end
+        Creature.eatUp(o,Int(var_209))
+        Creature.stepRight(o)
+        Creature.stepRight(o)
+        var_280 = if haskey(o.mem,var_280)
+                o.mem[var_280]
+            else
+                var_280
+            end
+        Creature.stepDown(o)
+        var_280 = var_280 + var_280
+        o.mem[var_280] = var_280
+        for i::Int8 = 1:div(var_209,_LOOP_STEPS_DIVIDER)
+            Creature.stepLeft(o)
+        end
+        Creature.stepDown(o)
+        Creature.stepRight(o)
+        Creature.stepRight(o)
+        var_209 = var_209 * var_209
+        Creature.stepRight(o)
+        var_312 = var_312[1:if length(var_312) > length(var_312)
+                        0
+                    else
+                        length(var_312) - length(var_312)
+                    end]
+        for i::Int8 = 1:div(var_209,_LOOP_STEPS_DIVIDER)
+        end
+        Creature.eatRight(o,Int(var_209))
+        Creature.eatLeft(o,Int(var_209))
+        Creature.eatLeft(o,Int(var_209))
+        Creature.stepRight(o)
+        Creature.eatDown(o,Int(var_209))
+        Creature.stepLeft(o)
+        Creature.stepDown(o)
+        Creature.eatRight(o,Int(var_209))
+        var_209 = var_209 / var_209
+        Creature.stepRight(o)
+        var_280 = if haskey(o.mem,var_280)
+                o.mem[var_280]
+            else
+                var_280
+            end
+        Creature.stepRight(o)
+        Creature.stepLeft(o)
+        Creature.eatRight(o,Int(var_209))
+        Creature.stepRight(o)
+        Creature.eatUp(o,Int(var_209))
+        Creature.stepRight(o)
+        Creature.eatRight(o,Int(var_209))
+        Creature.stepRight(o)
+        Creature.stepRight(o)
+        Creature.stepDown(o)
+        return var_209
+    end
+    local var_193::Bool = true
+    local var_172::Int16 = 5674
+    function func_155(var_151::Int64=1916579168657257381,var_152::Bool=true,var_153::Int16=-11790,var_154::String="c4bSsByK")
+        local var_335::Int16 = 12526
+        if var_152 > var_152
+            Creature.stepRight(o)
+            Creature.stepRight(o)
+        end
+        var_335 = if haskey(o.mem,var_153)
+                o.mem[var_153]
+            else
+                var_335
+            end
+        o.mem[var_153] = var_153
+        var_154 = var_154[if length(var_154) > length(var_154)
+                        1
+                    else
+                        length(var_154)
+                    end:if length(var_154) > length(var_154) > 0
+                        0
+                    else
+                        end
+                    end]
+        var_153 = if haskey(o.mem,var_335)
+                o.mem[var_335]
+            else
+                var_153
+            end
+        Creature.stepUp(o)
+        var_152 = var_152 * var_152
+        var_151 = var_151 / var_151
+        Creature.stepDown(o)
+        return var_151
+    end
+    local var_99::Bool = true
+    function func_79(var_78::Int8=48)
+        Creature.stepUp(o)
+        Creature.stepLeft(o)
+        Creature.eatUp(o,Int(var_78))
+        return var_78
+    end
+    function func_64(var_62::Int8=46,var_63::Bool=true)
+        local var_250::String = "KEOHcSRn"
+        local var_138::Int16 = 14102
+        local var_136::Int8 = 79
+        local var_117::Int8 = 111
+        var_138 = if haskey(o.mem,var_138)
+                o.mem[var_138]
+            else
+                var_138
+            end
+        var_117 = var_136 - var_117
+        Creature.eatDown(o,Int(var_136))
+        var_250 = var_250 * var_250
+        var_117 = var_136 * var_136
+        var_138 = var_138 / var_138
+        if var_138 >= var_138
+            Creature.stepDown(o)
+            Creature.stepDown(o)
+            Creature.stepLeft(o)
+        end
+        Creature.eatLeft(o,Int(var_62))
+        var_138 = if haskey(o.mem,var_138)
+                o.mem[var_138]
+            else
+                var_138
+            end
+        Creature.stepDown(o)
+        Creature.eatDown(o,Int(var_117))
+        Creature.stepLeft(o)
+        var_250 = var_250[if length(var_250) > length(var_250)
+                        1
+                    else
+                        length(var_250)
+                    end:if length(var_250) > length(var_250) > 0
+                        0
+                    else
+                        end
+                    end]
+        Creature.eatRight(o,Int(var_62))
+        Creature.eatLeft(o,Int(var_136))
+        Creature.stepLeft(o)
+        var_62 = var_136 / var_136
+        Creature.stepDown(o)
+        Creature.stepDown(o)
+        Creature.stepRight(o)
+        Creature.stepDown(o)
+        Creature.stepDown(o)
+        Creature.stepDown(o)
+        Creature.stepLeft(o)
+        Creature.stepDown(o)
+        Creature.eatLeft(o,Int(var_62))
+        Creature.stepLeft(o)
+        Creature.stepRight(o)
+        var_136 = var_62 / var_62
+        Creature.stepDown(o)
+        return var_62
+    end
+    local var_60::Int16 = -3687
+    function func_58(var_56::Int16=-25752,var_57::Int8=95)
+        local var_308::Int8 = 48
+        local var_285::String = "bKkg6RxN"
+        local var_188::Int16 = -18042
+        local var_162::Bool = false
+        local var_76::Int8 = 121
+        o.mem[var_56] = var_188
+        Creature.eatUp(o,Int(var_57))
+        Creature.stepUp(o)
+        var_162 = var_162 * var_162
+        var_285 = var_285[if length(var_285) > length(var_285)
+                        1
+                    else
+                        length(var_285)
+                    end:if length(var_285) > length(var_285) > 0
+                        0
+                    else
+                        end
+                    end]
+        Creature.eatLeft(o,Int(var_76))
+        Creature.eatDown(o,Int(var_57))
+        Creature.eatRight(o,Int(var_57))
+        Creature.eatDown(o,Int(var_76))
+        if var_56 === var_56
+            Creature.stepDown(o)
+            Creature.stepDown(o)
+            Creature.stepDown(o)
+            Creature.stepDown(o)
+        end
+        Creature.eatDown(o,Int(var_76))
+        Creature.eatLeft(o,Int(var_57))
+        Creature.eatRight(o,Int(var_57))
+        var_56 = if haskey(o.mem,var_188)
+                o.mem[var_188]
+            else
+                var_56
+            end
+        Creature.stepLeft(o)
+        Creature.eatUp(o,Int(var_76))
+        Creature.stepUp(o)
+        Creature.eatUp(o,Int(var_76))
+        if var_57 === var_57
+            Creature.stepLeft(o)
+        end
+        Creature.eatLeft(o,Int(var_57))
+        var_57 = var_308 * var_308
+        if var_76 <= var_76
+            local var_296::Int16 = -999
+            local var_288::Int64 = 6712236255001670110
+            local var_181::Int8 = 116
+            o.mem[var_296] = var_296
+            Creature.eatDown(o,Int(var_181))
+            Creature.stepLeft(o)
+            Creature.stepDown(o)
+            Creature.eatRight(o,Int(var_181))
+            Creature.eatLeft(o,Int(var_181))
+            Creature.stepLeft(o)
+            Creature.stepUp(o)
+            Creature.stepLeft(o)
+            Creature.stepRight(o)
+            var_245 = var_288 * var_288
+            var_296 = var_296 + var_296
+            var_181 = var_181 / var_181
+        end
+        Creature.stepRight(o)
+        if var_56 == var_56
+            local var_316::Bool = true
+            Creature.stepRight(o)
+            Creature.stepDown(o)
+            Creature.stepRight(o)
+            Creature.stepRight(o)
+        end
+        Creature.eatUp(o,Int(var_57))
+        Creature.eatUp(o,Int(var_308))
+        Creature.eatUp(o,Int(var_76))
+        var_162 = var_162 & var_162
+        Creature.eatRight(o,Int(var_76))
+        Creature.eatDown(o,Int(var_76))
+        Creature.eatUp(o,Int(var_76))
+        Creature.eatUp(o,Int(var_76))
+        Creature.eatUp(o,Int(var_76))
+        Creature.eatUp(o,Int(var_76))
+        return var_56
+    end
+    local var_17::Int8 = 47
+    Creature.stepUp(o)
+    var_60 = func_58(var_60)
+    Creature.stepDown(o)
+    Creature.eatRight(o,Int(var_17))
+    Creature.eatRight(o,Int(var_17))
+    var_242 = var_294 / var_294
+    Creature.stepUp(o)
+    if var_17 < var_17
+        Creature.stepDown(o)
+    end
+    var_242 = var_294 - var_242
+    Creature.eatRight(o,Int(var_17))
+    Creature.stepLeft(o)
+    Creature.eatLeft(o,Int(var_17))
+    var_99 = var_193 * var_193
+    var_60 = func_58(var_60)
+    var_60 = func_58(var_60,var_17)
+    var_60 = func_58(var_74)
+    Creature.stepUp(o)
+    var_60 = if haskey(o.mem,var_60)
+            o.mem[var_60]
+        else
+            var_60
+        end
+    Creature.stepUp(o)
+    var_242 = var_242 / var_242
+    var_193 = var_193 * var_193
+    Creature.stepRight(o)
+    if var_17 === var_17
+        Creature.stepUp(o)
+        Creature.stepLeft(o)
+        Creature.stepLeft(o)
+    end
+    var_60 = if haskey(o.mem,var_60)
+            o.mem[var_60]
+        else
+            var_60
+        end
+    Creature.eatDown(o,Int(var_17))
+    Creature.eatUp(o,Int(var_17))
+    var_17 = var_17 / var_17
+    var_60 = if haskey(o.mem,var_60)
+            o.mem[var_60]
+        else
+            var_60
+        end
+    var_242 = var_242 * var_242
+    Creature.stepLeft(o)
+    Creature.eatUp(o,Int(var_17))
+    Creature.stepLeft(o)
+    var_60 = var_60 / var_60
+    var_17 = func_64(var_17)
+    Creature.stepLeft(o)
+    Creature.stepLeft(o)
+    var_60 = func_58(var_172)
+    Creature.eatRight(o,Int(var_17))
+    var_242 = var_242 * var_294
+    var_17 = func_210(var_17)
+    var_99 = var_193 | var_193
+    Creature.stepRight(o)
+    Creature.eatUp(o,Int(var_17))
+    var_60 = func_58(var_60)
+    for i::Int8 = 1:div(var_17,_LOOP_STEPS_DIVIDER)
+        Creature.stepUp(o)
+    end
+    var_60 = var_60 * var_60
+    Creature.stepUp(o)
+    o.mem[var_60] = var_60
+    Creature.eatUp(o,Int(var_17))
+    var_294 = Int(Creature.getEnergy(o,Int(var_172),Int(var_60)))
+    o.mem[var_60] = var_60
+    return true
+end
