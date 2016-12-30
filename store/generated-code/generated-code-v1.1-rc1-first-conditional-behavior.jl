924ba67e550c7dea7522d270dce9de9265df20f2
DeadbraiN <deadbrainman@gmail.com>
2016-12-30 15:09:30 +0200

    This is it! First conditional behavior has born. See best organism's code:         var_33 = Creature.energyDown(o)         var_20 = var_20 * var_20         var_2 = if 127 >= abs(var_2)                 Int8(var_2)             else                 127             end         var_2 = if 127 >= abs(var_33)                 Int8(var_33)             else                 127             end         for i::Int8 = 1:div(var_2,16)             local var_36::Int8 = 29             var_36 = if 127 >= abs(1571)                     Int8(1571)                 else                     127                 end             Creature.eatDown(c,o,Int(var_36))         end

---
+++ store/generated-code/generated-code-v1.1-rc1-first-conditional-behavior.jl
@@ -0,0 +1,90 @@
+RpcApi.SimpleOrganism[RpcApi.SimpleOrganism(0x0000000000090ddf,:(function (c::Config.ConfigData,o::Creature.Organism)
+        local var_33::Int64 = 3761244718335218341
+        function func_31(var_29::String="kxAJlYap",var_30::Int16=6327)
+            o.mem[var_30] = var_30
+            Creature.stepLeft(o)
+            return var_29
+        end
+        local var_28::String = "BI2aHdRl"
+        local var_20::String = "ozWRZlps"
+        function func_7(var_6::String="ZKRF9L8v")
+            local var_37::String = "AaTltb79"
+            Creature.stepDown(o)
+            return var_6
+        end
+        var_33 = (Int64)(var_33) | (Int64)(var_33)
+        var_33 = Creature.energyDown(o)
+        var_33 = Creature.energyUp(o)
+        var_2 = if 127 >= 0.385419
+                Int8(round(0.385419))
+            else
+                127
+            end
+        for i::Int8 = 1:div(var_2,16)
+        end
+        var_2 = if 127 >= 0.388149
+                Int8(round(0.388149))
+            else
+                127
+            end
+        var_2 = if 127 >= abs(30028)
+                Int8(30028)
+            else
+                127
+            end
+        Creature.eatDown(c,o,Int(var_2))
+        var_33 = Int(Creature.idRight(o))
+        var_33 = if isempty(var_20)
+                0
+            else
+                1
+            end
+        Creature.eatDown(c,o,Int(var_2))
+        Creature.eatDown(c,o,Int(var_2))
+        Creature.eatRight(c,o,Int(var_2))
+        var_33 = Creature.energyUp(o)
+        Creature.eatUp(c,o,Int(var_2))
+        Creature.eatLeft(c,o,Int(var_2))
+        var_33 = Creature.energyUp(o)
+        Creature.eatRight(c,o,Int(var_2))
+        Creature.eatUp(c,o,Int(var_2))
+        var_33 = Creature.energyUp(o)
+        var_20 = string(24637)
+        Creature.eatUp(c,o,Int(var_2))
+        var_20 = string(var_20)
+        var_33 = Int(Creature.idLeft(o))
+        Creature.eatUp(c,o,Int(var_2))
+        Creature.eatRight(c,o,Int(var_2))
+        Creature.eatLeft(c,o,Int(var_2))
+        var_33 = Creature.energyUp(o)
+        var_33 = Creature.energyLeft(o)
+        var_20 = func_7(var_28)
+        var_33 = Creature.energyLeft(o)
+        var_33 = Creature.energyDown(o)
+        var_20 = var_20 * var_20
+        var_2 = if 127 >= abs(var_2)
+                Int8(var_2)
+            else
+                127
+            end
+        var_2 = if 127 >= abs(var_33)
+                Int8(var_33)
+            else
+                127
+            end
+        for i::Int8 = 1:div(var_2,16)
+            local var_36::Int8 = 29
+            var_36 = if 127 >= abs(1571)
+                    Int8(1571)
+                else
+                    127
+                end
+            Creature.eatDown(c,o,Int(var_36))
+        end
+        var_13 = Int16(Creature.eatDown(c,o,Int(var_2)))
+        var_20 = string(-3373)
+        Creature.eatRight(c,o,Int(var_2))
+        var_33 = Int(Creature.idLeft(o))
+        Creature.eatDown(c,o,Int(var_2))
+        return true
+    end),[98,38,30,73,83,1,72,0,51],47.4821,0,31.5481,298580,0x0000006b,Dict(542=>542,540=>540,546=>546,545=>545,544=>544,6327=>6327,541=>541,543=>543),[557,490],13144,51,53)
