a006571afc98a000460016ad3bf0eaf7fdd1160a
DeadbraiN <deadbrainman@gmail.com>
2017-02-21 01:07:29 +0200

    coloring... #35

--- src/manager/plugins/Status.jl
+++ src/manager/plugins/Status.jl
@@ -97,57 +97,51 @@ module Status
 
     if stamp - st.stamp >= cfg.modeStatusPeriod
       print(string(Dates.format(now(), "HH:MM:SS"), " "))
+      print_with_color(:green,  rpad(string("orgs:",   length(man.tasks)),               9))
+      print_with_color(:normal, rpad(string("ips:",    cfg.worldIps),                    9))
+      print_with_color(:green,  rpad(string("nrg:",    format(st.energy, commas=true)), 16))
       if cfg.modeStatusFull
-        print_with_color(:green,   rpad(string("eat:",    st.eatl),                     11))
-        print_with_color(:green,   rpad(string(st.eatr),                                 8))
-        print_with_color(:green,   rpad(string(st.eatu),                                 8))
-        print_with_color(:green,   rpad(string(st.eatd),                                 8))
-        print_with_color(:green,   rpad(string(format(st.eated, commas=true)),          10))
+        print_with_color(:red,   rpad(string("eat:",    st.eatl),                     11))
+        print_with_color(:red,   rpad(string(st.eatr),                                 8))
+        print_with_color(:red,   rpad(string(st.eatu),                                 8))
+        print_with_color(:red,   rpad(string(st.eatd),                                 8))
+        print_with_color(:red,   rpad(string(format(st.eated, commas=true)),          10))
       else
-        print_with_color(:green,   rpad(string("eat:", format(st.eated, commas=true)),  14))
+        print_with_color(:red,   rpad(string("eat:", format(st.eated, commas=true)),  14))
       end
-
       if cfg.modeStatusFull
-        print_with_color(:red,     rpad(string("step:", st.stepl),                      11))
-        print_with_color(:red,     rpad(string(st.stepr),                                6))
-        print_with_color(:red,     rpad(string(st.stepu),                                6))
-        print_with_color(:red,     rpad(string(st.stepd),                                6))
-        print_with_color(:red,     rpad(string(format(st.steps, commas=true)),          10))
+        print_with_color(:red, rpad(string("step:", st.stepl),                      11))
+        print_with_color(:red, rpad(string(st.stepr),                                6))
+        print_with_color(:red, rpad(string(st.stepu),                                6))
+        print_with_color(:red, rpad(string(st.stepd),                                6))
+        print_with_color(:red, rpad(string(format(st.steps, commas=true)),          10))
       else
-        print_with_color(:red,     rpad(string("step:", format(st.steps, commas=true)), 14))
+        print_with_color(:red, rpad(string("step:", format(st.steps, commas=true)), 14))
       end
-
-      print_with_color(:yellow,    rpad(string("ips:",    cfg.worldIps),                 9))
-      print_with_color(:yellow,    rpad(string("mut:",    st.mps),                       9))
-      print_with_color(:yellow,    rpad(string("kil:",    st.kops),                      8))
-      print_with_color(:yellow,    rpad(string("clon:",   st.cps),                       8))
+      print_with_color(:red,       rpad(string("mut:",    st.mps),                       9))
+      print_with_color(:red,       rpad(string("kil:",    st.kops),                      8))
+      print_with_color(:yellow,    rpad(string("clon:",   st.cps),                       9))
       print_with_color(:yellow,    rpad(string("eval:",   cfg.orgEvals - st.evals),     10))
       print_with_color(:yellow,    rpad(string("req:",    st.rps),                       9))
       if cfg.modeStatusFull
         print_with_color(:yellow,  rpad(string("sreq:",   st.srps),                      9))
         print_with_color(:yellow,  rpad(string("enup:",   st.eups),                      8))
       end
-      #if cfg.modeStatusFull print("\n         ") end
-
       print_with_color(:orange,    rpad(string("err:",    man.cfg.orgErrors),           11))
       if cfg.modeStatusFull
         print_with_color(:orange,    rpad(string("code:",   st.csMin),                  12))
         print_with_color(:orange,    rpad(string(st.csMax),                              7))
       else
-        print_with_color(:orange,    rpad(string("code:", div(st.csMax - st.csMin, 2)),  9))
+        print_with_color(:orange,    rpad(string("code:", div(st.csMax - st.csMin, 2)),  9), "\n")
       end
       if cfg.modeStatusFull
         print_with_color(:orange,    rpad(string("nrg:",    st.eMin),                   11))
-        print_with_color(:orange,    rpad(string(format(st.eMax, commas=true)),          7))
+        print_with_color(:orange,    rpad(string(format(st.eMax, commas=true)),          7), "\n")
       end
-      #if cfg.modeStatusFull print("                                                        ") end
 
       #print_with_color(:red,    rpad(string("ytps:",   st.ytps)), 11))
       #print_with_color(:red,    rpad(string("yps:",    st.yps)),  9))
       #print_with_color(:red,    rpad(string("syps:",   st.syps)),  9))
-
-      print_with_color(:cyan,   rpad(string("orgs:",   length(man.tasks)),               9))
-      print_with_color(:cyan,   rpad(string("nrg:",    format(st.energy, commas=true)),  16), "\n")
 
       st.stamp  = stamp
       st.ytps   = 0
