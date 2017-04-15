04a44f4a31d216f193b65f646da92f11a8288f50
DeadbraiN <deadbrainman@gmail.com>
2017-04-16 00:08:50 +0300

    updated tests for plus(), minus() operators. #113

--- test/test-CodeMath.jl
+++ test/test-CodeMath.jl
@@ -10,13 +10,13 @@ module TestCodeMath
   #
   # Tests operator plus() with specified type
   #
-  function testPlusOperatorWithType(typ::DataType, plusSym::Symbol)
+  function testOperatorWithType(typ::DataType, opSym::Symbol, op::Symbol = :plus)
     local d = create([Helper.Point(1,1)], Dict{Symbol, Any}())
     local org = d.orgs[1]
     local types = changeTypes([typ])
 
-    code(d, :plus, org)
-    @fact Helper.getArg(org.code, [2,31,2,1]) --> plusSym
+    code(d, op, org)
+    @fact Helper.getArg(org.code, [2,31,2,1]) --> opSym
     @fact eval(org.code)(d.cfg, org) --> true
     #
     # revert supported types
@@ -40,12 +40,12 @@ module TestCodeMath
     Manager.destroy(d.man)
   end
   facts("Testing CodeMath.plus() with different types") do
-    testPlusOperatorWithType(Int8, :(+))
-    testPlusOperatorWithType(Int16, :(+))
-    testPlusOperatorWithType(Int, :(+))
-    testPlusOperatorWithType(String, :(*))
-    testPlusOperatorWithType(Bool, :(&))
-    testPlusOperatorWithType(Float64, :(+))
+    testOperatorWithType(Int8, :(+))
+    testOperatorWithType(Int16, :(+))
+    testOperatorWithType(Int, :(+))
+    testOperatorWithType(String, :(*))
+    testOperatorWithType(Bool, :(&))
+    testOperatorWithType(Float64, :(+))
   end
   # facts("Testing CodeMath.plus() with String and Bool variables") do
   #   local conf  = Config.create()
@@ -107,102 +107,29 @@ module TestCodeMath
   #   #
   #   resetTypes(types)
   # end
-  # #
-  # # minus
-  # #
-  # facts("Testing CodeMath.minus() with different variable types") do
-  #   local conf = Config.create()
-  #   local org  = Creature.create(conf)
-  #   local typesAmount = length(Helper.SUPPORTED_TYPES)
   #
-  #   addVars(org, [2], Helper.CodePos(1,1,1))
-  #   Mutator._onAdd(conf, org, Helper.CodePos(1,1,typesAmount + 1), Code.CodePart(Code.minus, false))
+  # minus
   #
-  #   @fact length(Helper.getLines(org.code, [2])) --> typesAmount + 2
-  #   @fact eval(org.code)(conf, org) --> true
-  # end
-  # facts("Testing CodeMath.minus() without variables") do
-  #   local conf = Config.create()
-  #   local org  = Creature.create(conf)
-  #
-  #   Mutator._onAdd(conf, org, Helper.CodePos(1,1,1), Code.CodePart(Code.minus, false))
-  #
-  #   @fact length(Helper.getLines(org.code, [2])) --> 1
-  #   @fact eval(org.code)(conf, org) --> true
-  # end
-  # facts("Testing CodeMath.minus() with Int8 variable") do
-  #   local conf  = Config.create()
-  #   local org   = Creature.create(conf)
-  #   local types = changeTypes([Int8])
-  #
-  #   addVar(org, [2], Helper.CodePos(1,1,1), Int8)
-  #   Mutator._onAdd(conf, org, Helper.CodePos(1,1,2), Code.CodePart(Code.minus, false))
-  #   var = Helper.getArg(org.code, [2,1,1,1,1])
-  #
-  #   @fact Helper.getArg(org.code, [2,2,1]) --> var
-  #   @fact Helper.getArg(org.code, [2,2,2,1]) --> :(-)
-  #   @fact Helper.getArg(org.code, [2,2,2,2]) --> var
-  #   @fact Helper.getArg(org.code, [2,2,2,3]) --> var
-  #   @fact eval(org.code)(conf, org) --> true
-  #   #
-  #   # revert supported types
-  #   #
-  #   resetTypes(types)
-  # end
-  # facts("Testing CodeMath.minus() with Int16 variable") do
-  #   local conf  = Config.create()
-  #   local org   = Creature.create(conf)
-  #   local types = changeTypes([Int16])
-  #
-  #   addVar(org, [2], Helper.CodePos(1,1,1), Int16)
-  #   Mutator._onAdd(conf, org, Helper.CodePos(1,1,2), Code.CodePart(Code.minus, false))
-  #   var = Helper.getArg(org.code, [2,1,1,1,1])
-  #
-  #   @fact Helper.getArg(org.code, [2,2,1]) --> var
-  #   @fact Helper.getArg(org.code, [2,2,2,1]) --> :(-)
-  #   @fact Helper.getArg(org.code, [2,2,2,2]) --> var
-  #   @fact Helper.getArg(org.code, [2,2,2,3]) --> var
-  #   @fact eval(org.code)(conf, org) --> true
-  #   #
-  #   # revert supported types
-  #   #
-  #   resetTypes(types)
-  # end
-  # facts("Testing CodeMath.minus() with Int variable") do
-  #   local conf  = Config.create()
-  #   local org   = Creature.create(conf)
-  #   local types = changeTypes([Int])
-  #
-  #   addVar(org, [2], Helper.CodePos(1,1,1), Int)
-  #   Mutator._onAdd(conf, org, Helper.CodePos(1,1,2), Code.CodePart(Code.minus, false))
-  #   var = Helper.getArg(org.code, [2,1,1,1,1])
-  #
-  #   @fact Helper.getArg(org.code, [2,2,1]) --> var
-  #   @fact Helper.getArg(org.code, [2,2,2,1]) --> :(-)
-  #   @fact Helper.getArg(org.code, [2,2,2,2]) --> var
-  #   @fact Helper.getArg(org.code, [2,2,2,3]) --> var
-  #   @fact eval(org.code)(conf, org) --> true
-  #   #
-  #   # revert supported types
-  #   #
-  #   resetTypes(types)
-  # end
-  # facts("Testing CodeMath.minus() with String variable") do
-  #   local conf  = Config.create()
-  #   local org   = Creature.create(conf)
-  #   local types = changeTypes([String])
-  #
-  #   addVar(org, [2], Helper.CodePos(1,1,1), String)
-  #   Mutator._onAdd(conf, org, Helper.CodePos(1,1,2), Code.CodePart(Code.minus, false))
-  #   var = Helper.getArg(org.code, [2,2,1])
-  #
-  #   @fact Helper.getArg(org.code, [2,2,2,1]) --> var
-  #   @fact eval(org.code)(conf, org) --> true
-  #   #
-  #   # revert supported types
-  #   #
-  #   resetTypes(types)
-  # end
+  facts("Testing CodeMath.minus() with different types") do
+    testOperatorWithType(Int8, :(-), :minus)
+    testOperatorWithType(Int16, :(-), :minus)
+    testOperatorWithType(Int, :(-), :minus)
+    testOperatorWithType(Float64, :(-), :minus)
+  end
+  facts("Testing CodeMath.minus() with String variable") do
+    local d = create([Helper.Point(1,1)], Dict{Symbol, Any}())
+    local org = d.orgs[1]
+    local types = changeTypes([String])
+
+    code(d, :minus, org)
+    @fact length(Helper.getLines(org.code, [2])) --> 32
+    @fact eval(org.code)(d.cfg, org) --> true
+    #
+    # revert supported types
+    #
+    resetTypes(types)
+    Manager.destroy(d.man)
+  end
   # facts("Testing CodeMath.minus() with Bool variable") do
   #   local conf  = Config.create()
   #   local org   = Creature.create(conf)
