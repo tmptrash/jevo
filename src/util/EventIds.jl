1fa0f0c76ee76cc140318f5723682e51052c39ec
tmptrash <deadbrainman@gmail.com>
2017-05-14 23:41:01 +0300

    added missed file

---
+++ src/util/EventIds.jl
@@ -0,0 +1,44 @@
+#
+# List of all available event ids. New events should be added to
+# the end of the list.
+#
+# @author DeadbraiN
+#
+const EVENT_YIELD           = 1
+const EVENT_ITERATION       = 2
+const EVENT_IPS             = 3
+const EVENT_REQUEST         = 4
+const EVENT_BACKUP          = 5
+const EVENT_YIELDTO         = 6
+const EVENT_ORGANISM        = 7
+const EVENT_GRAB_ENERGY     = 8
+const EVENT_UPDATE_ENERGY   = 9
+const EVENT_KILL_ORGANISM   = 10
+const EVENT_MUTATIONS       = 11
+const EVENT_CLONE           = 12
+const EVENT_EAT_LEFT        = 13
+const EVENT_EAT_RIGHT       = 14
+const EVENT_EAT_UP          = 15
+const EVENT_EAT_DOWN        = 16
+const EVENT_STEP_LEFT       = 17
+const EVENT_STEP_RIGHT      = 18
+const EVENT_STEP_UP         = 19
+const EVENT_STEP_DOWN       = 20
+const EVENT_EAT_ORGANISM    = 21
+const EVENT_EAT_ENERGY      = 22
+const EVENT_BORN_ORGANISM   = 23
+const EVENT_DOT_REQUEST     = 24
+const EVENT_STEP_YIELD      = 25
+const EVENT_BEFORE_RESPONSE = 26
+const EVENT_AFTER_REQUEST   = 27
+const EVENT_GET_ENERGY      = 28
+const EVENT_PROP_LEFT       = 29
+const EVENT_PROP_RIGHT      = 30
+const EVENT_PROP_UP         = 31
+const EVENT_PROP_DOWN       = 32
+const EVENT_DOT             = 33
+const EVENT_MOVE            = 34
+const EVENT_GRAB_LEFT       = 35
+const EVENT_GRAB_RIGHT      = 36
+const EVENT_GRAB_UP         = 37
+const EVENT_GRAB_DOWN       = 38
